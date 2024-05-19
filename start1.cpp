e63aab5101fa6cf2a6210f4c8b51dd6cc46763f7
Aman Mishra <amanamanram108@gmail.com>
2024-05-19 22:38:26 +0530

    first commit

---
+++ start1.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+#define ll long long int
+#define lp(i,a,b) for(ll i=a;i<b;i++)
+#define lpa(i,a,b,x) for(ll i=a;i<b;i+=x)
+#define pl(i,a,b) for(ll i=a;i>=b;i--)
+#define eb push_back
+#define ms(a,x) memset(a,x,sizeof(a))
+#define all(v) v.begin(),v.end()
+#define pll pair<ll,ll>
+#define vpll vector<pll>
+#define vll vector<ll>
+#define vvll vector<vll>
+#define yes cout<<"YES\n";
+#define no cout<<"NO\n";
+#define ff first
+#define ss second
+const ll mod1= 1000000007;
+const ll mod2= 998244353;
+const ll INF= 5000000000000000000;
+const ll INM= -5000000000000000000;
+using namespace std;
+void solve()
+{
+   ll n;cin>>n;
+   ll a[n];
+   lp(i,0,n)cin>>a[i];
+}
+int main() 
+{
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  cout.tie(0);
+  ll t;
+  cin>>t;
+  lp(i,1,t+1)solve();
+}
